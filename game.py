ba0496238a8900cf15024d7aeda4fee83c4750e9
coreyapat <coreyapat19@gmail.com>
2021-06-17 10:19:07 -0500

    finished my rules

--- game.py
+++ game.py
@@ -38,26 +38,61 @@ class Game:
                     print("Tie")
                 elif self.player_two.chosen_gesture == "scissor":
                     print("Player Two Wins!")
-                    self.player_two + + self.score
+                    self.player_two ++ self.score
                 elif self.player_two.chosen_gesture == "lizard":
                     print("Player Two Wins!")
-                    self.player_two + + self.score
+                    self.player_two ++ self.score
                 elif self.player_two.chosen_gesture == "spock":
                     print("Player One Wins!")
-                    self.player_one + + self.score
+                    self.player_one ++ self.score
 
+            if self.player_one.chosen_gesture == "scissor":
+                if self.player_two.chosen_gesture == "rock":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
+                elif self.player_two.chosen_gesture == "paper":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
+                elif self.player_two.chosen_gesture == "scissor":
+                    print("Tie")
+                elif self.player_two.chosen_gesture == "lizard":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
+                elif self.player_two.chosen_gesture == "spock":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
 
+            if self.player_one.chosen_gesture == "lizard":
+                if self.player_two.chosen_gesture == "rock":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
+                elif self.player_two.chosen_gesture == "paper":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
+                elif self.player_two.chosen_gesture == "scissor":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
+                elif self.player_two.chosen_gesture == "lizard":
+                    print("Tie")
+                elif self.player_two.chosen_gesture == "spock":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
 
-
-
-
-
-
-
-
-
-
-
+            if self.player_one.chosen_gesture == "spock":
+                if self.player_two.chosen_gesture == "rock":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
+                elif self.player_two.chosen_gesture == "paper":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
+                elif self.player_two.chosen_gesture == "scissor":
+                    print("Player One Wins!")
+                    self.player_one ++ self.score
+                elif self.player_two.chosen_gesture == "lizard":
+                    print("Player Two Wins!")
+                    self.player_two ++ self.score
+                elif self.player_two.chosen_gesture == "spock":
+                    print("Tie")
 
 
     def choose_game_type(self):
--- human.py
+++ human.py
@@ -6,10 +6,17 @@ class Human(Player):
 
 
     def  the_gesture(self):
-        print ("Hey, make sure you pick 0 for rock, ")
+        print ("Hey, make sure you pick 0 for rock,1 for paper, 2 for scissor, 3 for lizard, 4 for spock ")
         choose_gesture = input()
         if choose_gesture == "0":
             self.chosen_gesture = self.gestures[0]
         elif choose_gesture == "1":
             self.chosen_gesture = self.gestures[1]
+        elif choose_gesture == "2":
+            self.chosen_gesture = self.gestures[2]
+        elif choose_gesture == "3":
+            self.chosen_gesture = self.gestures[3]
+        elif choose_gesture == "4":
+            self.chosen_gesture = self.gestures[4]
+
 
